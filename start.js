057c9a6f3283649932491cccaa2838865d85a078
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-13 16:28:51 -0700

    Set up mongoose connection and start Express server

--- start.js
+++ start.js
@@ -1,1 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection
+  .on("open", () => {
+    console.log("Mongoose connection open");
+  })
+  .on("error", (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+const app = require("./app");
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
