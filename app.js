917931bd8b2742190957239fc54fb8c69b5a1b28
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-15 17:31:17 -0700

    Enable body-parser middleware for URL-encoded form data in app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
